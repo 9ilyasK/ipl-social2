0a4876521643b3e6e046dad31b63139f9d076845
Ilyas Koklis <ilyas.koklis@student.vinci.be>
2026-01-16 16:43:01 +0100

    TDD: Test 5 - Règles du point domaine

--- email.test.ts
+++ email.test.ts
@@ -12,9 +12,14 @@ describe('Validation Email TDD', () => {
     test('Doit rejeter si pas de @', () => {
         expect(validerEmail("elevevinci.be")).toBe(false);
     });
-    
+
     test('Doit rejeter si rien avant ou après le @', () => {
         expect(validerEmail("@vinci.be")).toBe(false);
         expect(validerEmail("eleve@")).toBe(false);
+    });
+
+    test('Doit avoir un point dans le domaine mais pas à la fin', () => {
+        expect(validerEmail("eleve@vinci")).toBe(false);
+        expect(validerEmail("eleve@vinci.")).toBe(false);
     });
 });

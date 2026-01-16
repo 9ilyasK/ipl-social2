ce52cad8d211d106720343cc651a97f90d4130d5
Ilyas Koklis <ilyas.koklis@student.vinci.be>
2026-01-16 16:42:26 +0100

    TDD: Code 4 - Vérification structure avant/après @

--- email.ts
+++ email.ts
@@ -1,7 +1,12 @@
 export function validerEmail(email: string): boolean {
     if (email.includes(" ")) return false;
+    if (email.indexOf("@") === -1) return false;
+
+    const parties = email.split("@");
+    if (parties.length !== 2) return false; 
+    const [avant, apres] = parties;
     
-    if (email.indexOf("@") === -1) return false; // Ajout
-    
+    if (avant.length === 0 || apres.length === 0) return false;
+
     return true;
 }
